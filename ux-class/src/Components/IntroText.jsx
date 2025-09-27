64066dbcfa0d77c264a58320643f5af177ca3674
KS <180429671+ks9927@users.noreply.github.com>
2025-09-27 19:36:26 -0400

    created introText comp and added it to home page

---
+++ ux-class/src/Components/IntroText.jsx
@@ -0,0 +1,9 @@
+
+export default function IntroText({ title, description }) {
+    return(
+        <div className="text-center">
+            <h2 className="text-2xl font-bold mb-2">{title}</h2>
+            <p className="mx-28">{description}</p>
+        </div>
+    )
+}
--- ux-class/src/Pages/Home.jsx
+++ ux-class/src/Pages/Home.jsx
@@ -1,10 +1,20 @@
 import ImageCarousel from "../Components/ImageCarousel"
+import IntroText from "../Components/IntroText"
 
 export default function Home() {
     return(
         <div className="flex flex-col">
          <ImageCarousel />
-        <h1>This is the Home page</h1>
+
+         <div>
+              <IntroText
+              title="Tahini"
+              description="Tahini is a small tropical island known for its beautiful beaches, lush rainforests, and a small volcano that rises in the center. The island is home to friendly communities who rely on fishing and farming to make a living. Indigenous peoples have lived there for generations, keeping their traditions alive and caring for the land. With its warm climate and natural beauty, Tahini is a peaceful place where nature and culture come together.">
+              </IntroText>
         </div>
+
+
+        </div>
+    
     )
 }
