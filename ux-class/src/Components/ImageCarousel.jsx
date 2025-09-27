28850ecaf075bbd1cc9aa1c20a3c6fbc5e6afb43
KS <180429671+ks9927@users.noreply.github.com>
2025-09-27 18:26:45 -0400

    add div for image carousel

---
+++ ux-class/src/Components/ImageCarousel.jsx
@@ -0,0 +1,10 @@
+
+
+export default function ImageCarousel() {
+    return(
+        <div className="border-black border-solid  border-2 rounded-2xl
+         bg-gray-200 mt-8 mb-8 place-self-center w-3xl h-64">
+            <p>This is content</p>
+        </div>
+    )
+}
--- ux-class/src/Pages/Home.jsx
+++ ux-class/src/Pages/Home.jsx
@@ -1,5 +1,10 @@
+import ImageCarousel from "../Components/ImageCarousel"
+
 export default function Home() {
     return(
+        <div className="flex flex-col">
+         <ImageCarousel />
         <h1>This is the Home page</h1>
+        </div>
     )
 }
