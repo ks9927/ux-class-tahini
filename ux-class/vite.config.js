ed92bb73cd39cc20891a4c7cc9e6c5453a44820e
KS <180429671+ks9927@users.noreply.github.com>
2025-09-30 15:32:25 -0400

    Revert "added pages stuff"
    This reverts commit 41e66bb1bb74b006ec56fcbb3a97ee78c23b2423.

--- ux-class/vite.config.js
+++ ux-class/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [
     react(),
   tailwindcss()],
-  base: '/ux-class-tahini>/'
 })
